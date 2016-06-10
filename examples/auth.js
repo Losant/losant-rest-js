847a9cf054af5e23ea7760c351077b2fd956bc11
Michael Kuehl <michael@losant.com>
2016-06-10 11:34:44 -0400

    update examples to match other repos

--- examples/auth.js
+++ examples/auth.js
@@ -1,27 +1,40 @@
-var losant = require('../lib');
-var api = losant.createClient();
+var api = require('losant-api-js');
+var client = api.createClient();
 
 // using callbacks
-api.auth.authenticateUser(
-  { credentials: { email: 'test@example.com', password: 'qwerqwer' } },
+client.auth.authenticateUser({ credentials:
+  { email: 'example@losant.com', password: 'your_password' } },
   function (err, data) {
     if (err) { return console.log(err.stack); }
     console.trace(data);
   }
 );
+/* Example user result
+ * {
+ *   token: 'an auth token string',
+ *   userId: 'theUserId'
+ * }
+ */
 
 // using promises
-api.auth
-  .authenticateDevice({
-    credentials: {
-      deviceId: '123456789123456789000000',
-      key: 'abcdefghijklmnopqrstuvwxyz',
-      secret: 'abcdefghijklmnopqrstuvwxyz'
-    }
-  })
-  .then(function (data) {
-    console.trace(data);
-  })
-  .catch(function (err) {
-    console.log(err.stack);
-  });
+client.auth.authenticateDevice({ credentials: {
+    deviceId: '123456789123456789000000',
+    key: 'abcdefghijklmnopqrstuvwxyz',
+    secret: 'abcdefghijklmnopqrstuvwxyz'
+  }
+})
+.then(function (data) {
+  console.trace(data);
+})
+.catch(function (err) {
+  console.log(err.stack);
+});
+/* Example device result
+ * {
+ *   applicationId: 'myAppId',
+ *   token: 'an auth token string',
+ *   restricted: false,
+ *   deviceId: 'myDeviceId',
+ *   deviceClass: 'standalone'
+ * }
+ */
---
+++ examples/list_devices.js
@@ -0,0 +1,50 @@
+var api = require('losant-api-js');
+
+var client = api.createClient({ accessToken: 'my_token' });
+client.devices.get({ applicationId: 'myAppId' })
+.then(function (data) {
+  console.trace(data);
+})
+.catch(function (err) {
+  console.log(err.stack);
+});
+/* Example result
+ * {
+ *   count: 1,
+ *   items: [ {
+ *     deviceClass: 'standalone',
+ *     connectionInfo: { connected: 0, time: '2016-06-01T17:16:02.324Z' },
+ *     name: 'Ruby Client Testing',
+ *     creationDate: '2016-01-31T17:58:57.541Z',
+ *     lastUpdated: '2016-05-31T14:47:32.288Z',
+ *     id: 'myDevId',
+ *     attributes: [
+ *       { name: 'string', dataType: 'string' },
+ *       { name: 'number', dataType: 'number' },
+ *       { name: 'boolean', dataType: 'boolean' }
+ *     ],
+ *     description: '',
+ *     _etag: '"174-u7/3je4oFyaKGePPbcHLqw"',
+ *     deviceId: 'myDevId',
+ *     tags: [],
+ *     applicationId: 'myAppId',
+ *     _type: 'device',
+ *     _links: {
+ *       application: { href: '/applications/myAppId' },
+ *       devices: { href: '/applications/myAppId/devices' },
+ *       self: { href: '/applications/myAppId/devices/myDevId' }
+ *      }
+ *   } ],
+ *   applicationId: 'myAppId',
+ *   perPage: 100,
+ *   page: 0,
+ *   sortField: 'name',
+ *   sortDirection: 'asc',
+ *   totalCount: 1,
+ *   _type: 'devices',
+ *   _links: {
+ *     application: { href: '/applications/myAppId' },
+ *     self: { href: '/applications/myAppId/devices' }
+ *   }
+ * }
+ */
