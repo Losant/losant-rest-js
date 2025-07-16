b276d5257d9e7c248ccbd014e053420d04cc02da
eeerin <thompsonerin42@gmail.com>
2025-07-16 10:06:15 -0400

    fixing linter issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,7 +43,7 @@ module.exports = {
             "error",
             "last"
         ],
-        "complexity": "error",
+        "complexity": ["error", { "max": 30 }],
         "computed-property-spacing": [
             "error",
             "never"
